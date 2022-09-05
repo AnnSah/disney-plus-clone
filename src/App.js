5ca38474463489a3439c2188478ec5333827c368
Anisa <anissa.sahiiti@gmail.com>
2022-09-05 04:03:20 +0200

    ADD: DetailPage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,31 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Switch from "react-router-dom/Switch"
+import Detail from './components/Detail';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/detail">
+            <Detail />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+      </Router>
     </div>
   );
 }
